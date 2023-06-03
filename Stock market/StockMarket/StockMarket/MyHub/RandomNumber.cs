0ac471fe51230f6aaf260c4d76ce04ef81ac93fc
Tarek Reda <tarekreda803@gmail.com>
2023-06-03 20:29:52 +0300

    updated

--- Stock market/StockMarket/StockMarket/MyHub/RandomNumber.cs
+++ Stock market/StockMarket/StockMarket/MyHub/RandomNumber.cs
@@ -1,15 +1,24 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using Newtonsoft.Json.Linq;
 using Stock_Models;
+using StockMarket.Data;
 using System.Threading.Tasks;
+using Stock_Models.Model;
 
 namespace StockMarket.MyHub
 {
     public class RandomNumber:Hub
     {
+        Context context;
+        public RandomNumber(Context context) 
+        {
+            this.context = context;
+        }
+
         public async Task SendRandomNumber()
         {
-            //List<Stock> stocks = C;
+            
+            List<Stock> stocks = context.Stocks.ToList();
             var random = new Random();
             var randomNumber = random.Next(1, 100);
             var timer = new Timer((state) => {
@@ -17,7 +26,7 @@ namespace StockMarket.MyHub
                 randomNumber = random.Next(-100, 101);
                 Console.WriteLine($"New value: {randomNumber}");
             }, null, TimeSpan.Zero, TimeSpan.FromSeconds(10));
-            await Clients.All.SendAsync("newRandomNumber", randomNumber);
+            await Clients.All.SendAsync("newRandomNumber", stocks);
         }
 
         //public async Task update()
